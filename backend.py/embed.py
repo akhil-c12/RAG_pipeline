c05b708ec166c7d1d0914796942bd2bd1c409a38
Chamala Akhileshwar Reddy <chamalaakhilreddy@gmail.com>
2026-03-01 16:21:31 +0530

    fix:vector store logi rewritten so that multiple pdf can be uploaded

--- backend.py/embed.py
+++ backend.py/embed.py
@@ -5,12 +5,18 @@ import os
 EMBEDDING_MODEL="sentence-transformers/all-MiniLM-L6-v2"
 FAISS_INDEX_PATH="vector_store"
 
-def create_vector_store(chunks):
+def create_vector_store(chunks,filename):
     embeddings=HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-    vector_store=FAISS.from_texts(texts=chunks,embedding=embeddings,metadatas=[{"source":"filename.pdf"}for _ in chunks])
-    os.makedirs(FAISS_INDEX_PATH, exist_ok=True)
-    vector_store.save_local(FAISS_INDEX_PATH)
-    return vector_store
+    vector_store=FAISS.from_texts(texts=chunks,embedding=embeddings,metadatas=[{"source":filename}for _ in chunks])
+    if os.path.exists(FAISS_INDEX_PATH):
+        existing_store=FAISS.load_local(FAISS_INDEX_PATH,embeddings,allow_dangerous_deserialization=True)
+        existing_store.merge_from(vector_store)
+        existing_store.save_local(FAISS_INDEX_PATH)
+        return existing_store
+    else:
+        os.makedirs(FAISS_INDEX_PATH,exist_ok=True)
+        vector_store.save_local(FAISS_INDEX_PATH)
+        return vector_store
 
 def Load_vector_store():
     embeddings=HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
