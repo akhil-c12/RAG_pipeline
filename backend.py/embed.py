54f7ba989ade2cbe2d3307d355655b2d16f66d8d
Chamala Akhileshwar Reddy <chamalaakhilreddy@gmail.com>
2026-03-01 16:13:43 +0530

    feat:created feature to store embeddings in vector store

--- backend.py/embed.py
+++ backend.py/embed.py
@@ -7,8 +7,12 @@ FAISS_INDEX_PATH="vector_store"
 
 def create_vector_store(chunks):
     embeddings=HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
-    vector_store=FAISS.from_texts(texts=chunks,embedding=embeddings)
+    vector_store=FAISS.from_texts(texts=chunks,embedding=embeddings,metadatas=[{"source":"filename.pdf"}for _ in chunks])
     os.makedirs(FAISS_INDEX_PATH, exist_ok=True)
     vector_store.save_local(FAISS_INDEX_PATH)
     return vector_store
+
+def Load_vector_store():
+    embeddings=HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
+    return FAISS.load_local(FAISS_INDEX_PATH, embeddings, allow_dangerous_deserialization=True)
 
